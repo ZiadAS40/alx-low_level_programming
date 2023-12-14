558b3346bee64c72e713725c63accce67f195ca2
ZiadAS40 <zead4036@gmail.com>
2023-12-14 12:21:37 -0800

    any

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main that a program that's prints the belew output 
+ *
+ * return :0
+ */
 int main(void) {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
 
