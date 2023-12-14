41a94efc7f5683daa77b3a10546f90da4ff7f65d
ZiadAS40 <132669885+ZiadAS40@users.noreply.github.com>
2023-12-14 22:30:36 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: Prints the message "with proper grammar,
- * but the outcome is a piece of art," using the printf function.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+#include <stdio.h>                                                                  
+/**                                                                                 
+ *  * Description: main - Prints out a string to stdout.                            
+ *   * Return: 0 if success.                                                        
+*/                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("with proper grammar, but the outcome is a piece of art,\n");                
+return (0);
 }
+
+
