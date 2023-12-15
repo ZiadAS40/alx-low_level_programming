eda9744bfbd553496a455d533059e461dc70838f
ZiadAS40 <zead4036@gmail.com>
2023-12-15 10:29:39 -0800

    any

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+** Description: main - Prints out a string to stdout.
+** Return: 0 if success.
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 10 ; ++m)
+	{
+		printf("%d\n", n);
+	}
+	return (0)
+}
