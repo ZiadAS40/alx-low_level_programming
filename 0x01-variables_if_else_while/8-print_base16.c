80673aa4096f150913f1a52492727cf80eb5d2ee
ZiadAS40 <zead4036@gmail.com>
2023-12-15 11:00:12 -0800

    any

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hexDigit;
+
+	for (hexDigit = '0'; hexDigit <= '9'; ++hexDigit)
+	{
+		putchar(hexDigit);
+	}
+
+	for (hexDigit = 'a'; hexDigit <= 'f'; ++hexDigit)
+	{
+		putchar(hexDigit);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
+
