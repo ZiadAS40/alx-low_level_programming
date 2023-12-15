63820ed8bef79a16546b6b44b399878da39bb824
ZiadAS40 <zead4036@gmail.com>
2023-12-15 09:49:59 -0800

    any

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main : any
+ * return : (0) if sucess
+ **/
+
+int main (void)
+{
+	int currentChar ;
+
+	for (currentChar = 'a' ; currentChar <= 'z' ; ++ currentChar)
+	{
+		putchar(currentChar);
+	}
+	putchar("\n");
+	return 0
+}
