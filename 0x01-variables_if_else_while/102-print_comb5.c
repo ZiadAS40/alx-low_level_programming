50654cddef9c7d8a44190118effae31e5c7f450b
ZiadAS40 <zead4036@gmail.com>
2023-12-15 11:22:07 -0800

    any

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+    int num1;
+    int num2;
 
-	for (num1 = 0; num1 <= 99; ++num1)
-	{
-		for (num2 = num1 + 1; num2 <= 99; ++num2)
-		{
-			putchar('0' + num1 / 10);
-			putchar('0' + num1 % 10);
-			putchar(' ');
-			putchar('0' + num2 / 10);
-			putchar('0' + num2 % 10);
+    for (num1 = 0; num1 <= 99; ++num1)
+    {
+        for (num2 = num1 + 1; num2 <= 99; ++num2)
+        {
+            putchar('0' + num1 / 10);
+            putchar('0' + num1 % 10);
+            putchar(' ');
+            putchar('0' + num2 / 10);
+            putchar('0' + num2 % 10);
 
-			if (num1 != 98 || num2 != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+            if (num1 != 98 || num2 != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return 0;
+    return 0;
 }
