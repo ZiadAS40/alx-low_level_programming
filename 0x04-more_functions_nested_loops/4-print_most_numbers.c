9d74eda0fdb11a9c7f217986ba33f60967f94091
ZiadAS40 <ziad403612@gmail.com>
2023-12-21 23:34:18 -0800

    any

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - Prints the numbers (0-9) except (2,4) followed by new line.
+ * print_numbers - Prints the numbers (0 - 9) except (2,4) followed by new line.
  *
  * Return: void
  */
