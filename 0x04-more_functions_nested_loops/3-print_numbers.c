ced3fc5cb5abbfb1d50d7971962ccfe3d0aa9b3b
ZiadAS40 <zead4036@gmail.com>
2023-12-21 22:13:41 -0800

    any

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the nunbers (0-9) followed by new line.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (int i = 48 ; i <= 57 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
