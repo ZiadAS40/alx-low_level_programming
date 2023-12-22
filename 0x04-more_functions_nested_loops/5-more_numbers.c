6071ae33613b906466b1a49b6320ac8fe02f8c7e
ZiadAS40 <ziad403612@gmail.com>
2023-12-21 23:40:25 -0800

    any

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 
 	for (j = '0' ; j <= '10' ; j++)
 	{
-	int i;
+		int i;
 
-	for (i = '0' ; i <= '14' ; i++)
-	{
+		for (i = '0' ; i <= '14' ; i++)
+		{
 		_putchar(i);
-	}
-	_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
