c73c078d5a49e1ba1a5b3f603c1b0c18db049c06
ZiadAS40 <ziad403612@gmail.com>
2023-12-22 00:03:09 -0800

    any

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-int _putchar(char c)
-{
-        putchar(c)
-};
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,3 +15,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+#endif
