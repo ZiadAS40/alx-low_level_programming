6ea4e6bd94bf2cae69e9259eda795b2180fe8aff
ZiadAS40 <ziad403612@gmail.com>
2023-12-23 04:22:56 -0800

    any

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,18 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n >=0)
+	{
 	for (i = 0; i < n ; i++)
 	{
 		_putchar(92);
+		if (i == n - 1)
 		_putchar('\n');
 		for (j = 0; j <= i; j++)
 		{
 			_putchar(32);
 		}
 	}
+	}else
+		_putchar('\n');
 }
