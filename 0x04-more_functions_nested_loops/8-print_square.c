bc8f0a2b01aedd110ecbb6f814277db6fda4a6a0
ZiadAS40 <ziad403612@gmail.com>
2023-12-22 23:56:14 -0800

    any

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - print a square of the number.
+ * @size: the number of the rows and the element on the row.
+ * Return: 0.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+		_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
