e0de29f572d94020badf6eeee56086108a032405
ZiadAS40 <zead4036@gmail.com>
2024-01-02 06:30:23 -0800

    any

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that resets the pointer to 98
+ * @m: the poiner that will assign the pointer to 98
+ * Retunr : always 0
+ */
+void reset_to_98(int *m)
+{
+	*m = 98;
+}
