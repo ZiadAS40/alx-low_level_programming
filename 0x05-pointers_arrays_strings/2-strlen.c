c9ac98c3139438f3a84c3d4d2287ace612df9d25
ZiadAS40 <zead4036@gmail.com>
2024-01-02 08:07:53 -0800

    any

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - a function that count the lengh of the string
+ * @s: the string that we should count
+ * Return: the lengh of the string
+ */
+int _strlen(int *s)
+{
+	int counter;
+
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		counter++
+		return (counter);
+	}
+}
