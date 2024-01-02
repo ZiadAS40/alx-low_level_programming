e5cb95f91f1f798a0b41da0c6e69966634d6b1f6
ZiadAS40 <zead4036@gmail.com>
2024-01-02 09:47:58 -0800

    any

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * print_rev - a function that print the string by revers
+ * @s: the string that will be reversed
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	for (s[i] != '\0'; i++)
+	{
+		if (s[i] != '\0')
+		{
+			j = i;
+			for (; j >= 0; j--)
+				printf("%c", s[j]);
+		}
+	}
+	printf("\n");
+}
