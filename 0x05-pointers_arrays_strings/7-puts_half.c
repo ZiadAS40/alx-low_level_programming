2f30b7258bb13a60588bf308bf394844444c0ab4
ZiadAS40 <zead4036@gmail.com>
2024-01-03 07:45:48 -0800

    any

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of the string
+ * @str: the string that will be printed
+ * Return:0
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	for (; str[i] != '\0'; i++)
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+	} else
+	{
+		j = (i / 2) + 1;
+	}
+	for (; j <= i; j++)
+	{
+		printf("%c", str[j]);
+	}
+	printf("\n");
+}
