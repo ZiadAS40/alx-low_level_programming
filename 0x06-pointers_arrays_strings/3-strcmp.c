0619dd78eea7aff09c78ede5dc333b96ed6520ad
ZiadAS40 <zead4036@gmail.com>
2024-01-04 10:33:56 -0800

    any

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,13 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	while (s1[i] != '\0')
+	while (j == 0)
 	{
-		i++;
+		if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+			{
+				break;
+			}
+		j = *(s1 + i) - *(s2 + i);
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i == j)
-	{
-		return (0);
-	} else if (i > j)
-	{
-		return (i - j);
-	} else if (i < j)
-	{
-		return (i - j);
-	}
+	return (j);
 }
