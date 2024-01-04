49a206d4a1e744d090e6dc826c0c5989156f3e44
ZiadAS40 <zead4036@gmail.com>
2024-01-04 08:21:47 -0800

    any

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - copieing a string in another with the limit n.
+ * @dest: distination string.
+ * @src: source string.
+ * @n: the bytes that should be copied.
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+	}
+}
