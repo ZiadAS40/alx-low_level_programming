5de14f612fc5b20c85dcb621be4f8f2110b07dcd
ZiadAS40 <zead4036@gmail.com>
2024-01-05 00:24:26 -0800

    any

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - captalize the first char in the word.
+ * @s: string.
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; j < 13 ; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				if (*(s + (i + 1)) >= 'a' && *(s + (i + 1)) <= 'z')
+				{
+				*(s + (i + 1)) = *(s + (i + 1)) - ('a' - 'A');
+				break;
+				}
+			}
+		}
+	}
+	return (s);
+}
