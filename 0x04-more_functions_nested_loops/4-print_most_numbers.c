118bb32a4340ccf6981280d056eb32c6568fbc57
ZiadAS40 <zead4036@gmail.com>
2024-01-06 22:00:20 -0800

    any

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - Prints the numbers (0 - 9) except (2,4) followed by '\n'.
+ * print_most_numbers - Prints the numbers (0 - 9) except (2,4) followed by '\n'.
  *
  * Return: void
  */
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 	{
 		if (i == '2' || i == '4')
 		{
-		continue;
+		break;
 		}
 		_putchar(i + 48);
 	}
