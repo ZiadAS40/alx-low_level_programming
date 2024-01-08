bbb5515f7e8c3557ae1115da59c138cc9a4baf4e
ZiadAS40 <zead4036@gmail.com>
2024-01-08 01:13:58 -0800

    any

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: the string.
+ * @c: the character.
+ * Return: a Pointer to the character.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
