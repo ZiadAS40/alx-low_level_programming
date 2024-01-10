a23ff626e3d9e135ec3eb4070e05b454bbb60c11
ZiadAS40 <zead4036@gmail.com>
2024-01-10 07:03:37 -0800

    any

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - a function that print the string in reverse order.
+ * @s: the string.
+ * Return: s.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	} else
+	{
+		_putchar('\n');
+	}
+}
