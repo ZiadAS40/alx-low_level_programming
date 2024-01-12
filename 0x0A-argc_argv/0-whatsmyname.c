360fd3b880a981762b306a96e116e1bf90e8c19e
ZiadAS40 <zead4036@gmail.com>
2024-01-12 06:41:36 -0800

    any

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a function that prints the file name.
+ * @argc: the number of arguments.
+ * @argv: an array that contains the arguments.
+ * Return: 0.
+ */
+void main(int argc, char *argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
