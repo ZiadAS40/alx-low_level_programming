aa86c03c6c36a6bb958d35809debcf3871c2bc1e
ZiadAS40 <zead4036@gmail.com>
2024-01-12 08:08:57 -0800

    any

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * mian - a function that multiply two numbers.
  * @argc: the number of arguments.
@@ -7,7 +8,7 @@
  */
 int mian(int argc, char *argv[])
 {
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("error\n");
 		return (1);
