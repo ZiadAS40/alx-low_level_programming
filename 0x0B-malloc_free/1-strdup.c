a8e205d981172ab0cc95d6a9be1446a2618fc942
ZiadAS40 <zead4036@gmail.com>
2024-01-14 22:39:06 -0800

    any

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - duplicated the string in a new memory location.
+ * @str: the string to be dulicated.
+ * Return: a pointer to anewly location.
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j;
+	char *a;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	a = malloc((i + 1) * sizeof(char));
+	for (j = 0; j < i; j++)
+	{
+		if (a == NULL)
+			return (0);
+		a[j] = str[j];
+	}
+	return (a);
+}
