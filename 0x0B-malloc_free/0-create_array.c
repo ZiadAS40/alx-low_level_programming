c745b82bcbafd65de07f70611ffaf8e2c9b56139
ZiadAS40 <zead4036@gmail.com>
2024-01-14 22:19:38 -0800

    any

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	char *a;
 
 	a = malloc(size * sizeof(char));
+	if (size == 0)
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		if (a == NULL)
-		{
-			return ("failed to allocate memory");
-		}
+			return (0);
 		a[i] = c;
 	}
 	return (a);
