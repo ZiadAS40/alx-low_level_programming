c7af389fb7676aa7add885efc15c7a766fc781f3
ZiadAS40 <zead4036@gmail.com>
2024-01-22 10:55:36 -0800

    any

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - assign a values to the strucure dog.
@@ -10,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	d->owner = owner;
+if (d != NULL)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
 }
