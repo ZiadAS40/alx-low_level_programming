4340dc1d9df4efa8f12e836c29087d4ef9181a20
ZiadAS40 <zead4036@gmail.com>
2024-01-22 09:57:01 -0800

    any

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - define a new structure.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of it.
+ *
+ * Description: a structure of a dog contains name, age and owner as members.
+ */
 struct dog
 {
 	char *name;
