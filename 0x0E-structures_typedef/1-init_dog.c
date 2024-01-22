1474b52017c12eb2397b8c54067c185aa2cf6912
ZiadAS40 <zead4036@gmail.com>
2024-01-22 10:30:01 -0800

    any

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	} else
+	{
 	(*d).name = name;
 	(*d).age = age;
 	d->owner = owner;
+	}
 }
