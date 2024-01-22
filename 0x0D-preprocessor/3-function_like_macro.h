03ecffb8b6236897e9e34c1a62b4cb4f7e3a540d
ZiadAS40 <zead4036@gmail.com>
2024-01-22 01:21:13 -0800

    any

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO.H
-#define FUNCTION_LIKE_MACRO.H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 #define ABS(x) ((x > 0) ? (x) : (x) * (-1))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,4 @@
-#ifndef SUM.H
-#define SUM.H
+#ifndef SUM_H
+#define SUM_H
 #define SUM(a, b) ((a) + (b))
 #endif
