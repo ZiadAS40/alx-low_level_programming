c3aade355152ac1fce335bab86ba508028df3c15
ZiadAS40 <zead4036@gmail.com>
2024-01-22 01:10:04 -0800

    any

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,3 +1,3 @@
-#define PI
+#ifdef PI
 #define PI 3.14159265359
 #endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,8 @@
  *
  * Return: always 0.
  */
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
