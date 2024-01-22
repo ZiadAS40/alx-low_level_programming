204686ddb5b26cfb260e422c82db0d2de8095d95
ZiadAS40 <zead4036@gmail.com>
2024-01-22 10:16:22 -0800

    any

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * init_dog - assign a values to the strucure dog.
+ * @d: the structure.
+ * @name: the name of the dog.
+ * @age: the age of it.
+ * @owner: it's owner.
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog;
+
+	d = &dog;
+	(*d).name = "Bob";
+	(*d).age = 3.5;
+	d->owner = "Ali";
+}
