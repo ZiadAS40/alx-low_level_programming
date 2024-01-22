de05d8a42a8a029ddeb0302aba03ac344077c2a5
ZiadAS40 <zead4036@gmail.com>
2024-01-22 00:36:03 -0800

    any

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,4 @@
+#ifndef 1-PI.h
+#define 1-PI.h
+#define PI 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file the compiled from.
+ *
+ * Return: always 0.
+ */
+void main(void)
+{
+	printf("%s\n", __FILE__);
+}
