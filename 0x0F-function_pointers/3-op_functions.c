51f7f4d4acd3c93cc1402e6cc0d1308516dab099
ZiadAS40 <zead4036@gmail.com>
2024-01-24 11:39:47 -0800

    any

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * get_op_func - determine which process to perform.
  * @s: operator.
@@ -19,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 6)
 	{
-		if (ops[i].op[0] == s[0] && s[1] == '\0')
+		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
 		i++;
 	}
