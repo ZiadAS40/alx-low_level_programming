f5b553844222dc7c98b4831475c344ccac55d12a
ZiadAS40 <zead4036@gmail.com>
2024-01-24 00:42:28 -0800

    any

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - search for intger.
+ * @array: array.
+ * @size: size of array.
+ * @cmp: function pointer.
+ * Return: the 1st elemnt that makes cmd true else -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		return (i);
+	}
+	return (-1);
+}
