bcde597f434f702adcfc808b224e71196aa624a6
ZiadAS40 <zead4036@gmail.com>
2024-01-24 01:28:32 -0800

    any

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,5 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
-/**
- * printn - a function that will be assigned to f pointer.
- * @a: a pointer to string.
- * Return: 0.
- */
-void printn(char *a)
-{
-	int i = 0;
-
-	while (a[i] != '\0')
-	{
-		putchar(a[i]);
-		i++;
-	}
-}
-
 /**
  * print_name - a function that prints name with function pointer.
  * @name: the name.
