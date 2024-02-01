abcb1f402fab843d29a32f34843436052f475398
ZiadAS40 <zead4036@gmail.com>
2024-02-01 14:28:12 +0200

    fixing some issues

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * befor_main - prints befor the excution of the main function.
+ * 
+ * Return: 0.
+*/
+
+void __attribute__ ((constructor)) befor_main()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
