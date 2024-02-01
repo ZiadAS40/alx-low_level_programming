995dcb3244d4e8b6d5d8fc87068e67298528d8a2
ZiadAS40 <zead4036@gmail.com>
2024-02-01 13:47:50 +0200

    Fix Some Issues

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *temp = malloc(sizeof(list_t));
-size_t ln;
+size_t ln = 0;
 if (temp == NULL)
 return (NULL);
 temp->str = strdup(str);
