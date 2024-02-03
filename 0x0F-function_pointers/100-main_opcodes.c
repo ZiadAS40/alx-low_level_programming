eeefadb35c4f057275236be1a7275ba0b4865a88
ZiadAS40 <zead4036@gmail.com>
2024-02-03 16:22:05 +0200

    adding advanced task file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ ** main - prints opcodes.
+ ** @argc: arguments (unknown)
+ ** @argv: argument (unknown)
+ **
+ ** Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	int b, i;
+	char *ar;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	ar = (char *)main;
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("%02hhx\n", ar[i]);
+			break;
+		}
+		printf("%02hhx ", ar[i]);
+	}
+	return (0);
+}
