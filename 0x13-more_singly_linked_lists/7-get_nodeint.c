d1a7d3198602d4f05c03fcbf29bff81e62d62ca1
ZiadAS40 <zead4036@gmail.com>
2024-02-06 08:44:37 +0200

    adding task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get the nth node.
+ * @head: listint_t.
+ * @index: usigned int.
+ * Return: listint_t *.
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+if (head != NULL)
+{
+while (i < index)
+{
+head = head->next;
+i++;
+}
+}
+return (head);
+}
