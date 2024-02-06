ecddd5b7263fcf39927597da4bec29a75a85d25e
ZiadAS40 <zead4036@gmail.com>
2024-02-06 09:04:52 +0200

    adding task number 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum all data in a list.
+ * @head: listint_t.
+ * Return: int.
+*/
+
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
