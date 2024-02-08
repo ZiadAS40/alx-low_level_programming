c69d9ef73c7220dbcae9b035a52c0e3bd10edd1a
ZiadAS40 <zead4036@gmail.com>
2024-02-08 17:24:28 +0200

    fix some issues

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,28 +11,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 
-if (_longPow(2, index) > n)
+if (index >= sizeof(unsigned long int) * 8)
 return (-1);
 if (n & (1UL << index))
 return (1);
 else
 return (0);
-}
-
-/**
- * _longPow - get the power on two numbers.
- * @base: intger.
- * @exp: intger.
- * Return: intger.
-*/
-unsigned long int _longPow(int base, int exp)
-{
-unsigned long int result  = 1;
-
-while (exp > 0)
-{
-result *= base;
-exp--;
-}
-return (result);
 }
