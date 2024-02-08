fb566151922be0a61adfd101923c547da57cdca5
ZiadAS40 <zead4036@gmail.com>
2024-02-08 14:43:00 +0200

    fix some issues

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,19 +13,21 @@ int get_bit(unsigned long int n, unsigned int index)
 unsigned long int powerTwo;
 
 if (index == 0)
-return (0);
-if (index == 1)
 {
-if (!n % 2)
+if (n % 2 == 0)
 return (0);
 else
 return (1);
 }
+
 powerTwo = _longPow(2, index);
+if (powerTwo > n)
+return (-1);
 if (n >= powerTwo)
 return (1);
 else
 return (0);
+return (-1);
 }
 
 /**
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -9,11 +9,11 @@ int main(void)
 {
     int n;
 
-    n = get_bit(1024, 10);
+    n = get_bit(1024, 11);
     printf("%d\n", n);
-    n = get_bit(98, 1);
+    n = get_bit(98, 0);
     printf("%d\n", n);
-    n = get_bit(1024, 0);
+    n = get_bit(1025, 0);
     printf("%d\n", n);
     return (0);
 }
