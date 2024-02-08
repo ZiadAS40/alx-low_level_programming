91d7fa11ce2deff8431dd96375e8d31452fc6b49
ZiadAS40 <zead4036@gmail.com>
2024-02-08 19:59:42 +0200

    adding task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - set the index bit to 1.
+ * @n: the number.
+ * @index: the index that will be modified to 1.
+ * Return: 1 on success
+ * -1 if fail.
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+*n = (*n | (1UL << index));
+return (1);
+}
