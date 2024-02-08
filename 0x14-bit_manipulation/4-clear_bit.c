333ed383f45db7d01de3449075e894b8ac7490d8
ZiadAS40 <zead4036@gmail.com>
2024-02-08 20:09:04 +0200

    add task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - set the index bit to 0.
+ * @n: the number.
+ * @index: the index of the bit.
+ * Return: 1 if succes
+ * -1 if fails.
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+if (*n & (1UL << index))
+{
+*n ^= (1UL << index);
+}
+return (1);
+}
