cba265d5b1940577ff21ed20d531559083920e66
ZiadAS40 <zead4036@gmail.com>
2024-02-12 18:47:27 +0200

    adding the first file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,8 +14,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytesWrite;
 	char buffer[letters];
 
-if (filename == NULL)
-return (0);
+	if (filename == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
@@ -31,7 +32,7 @@ return (0);
 	if (bytesWrite == -1)
 		return (0);
 
-	close(filename);
+	close(fd);
 	free(buffer);
 
 	return (bytesWrite);
