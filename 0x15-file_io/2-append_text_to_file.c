ef21435154e23fde634dc21ddee3591d43bffa94
ZiadAS40 <zead4036@gmail.com>
2024-02-12 20:45:12 +0200

    adding the 3rd task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content)
-	 write(fd, text_content, strlen(text_content));
-
+	{
+		write(fd, text_content, strlen(text_content));
+	}
 	return (1);
 }
