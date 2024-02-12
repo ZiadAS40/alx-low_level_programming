d2c29f8685ba8dfb7499b5e933c79383c864671c
ZiadAS40 <zead4036@gmail.com>
2024-02-12 18:46:16 +0200

    adding the first file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a context of a file.
+ * @filename: the name of the file.
+ * @letters: the number of letters.
+ * Return: the number of letters printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytesRead;
+	ssize_t bytesWrite;
+	char buffer[letters];
+
+if (filename == NULL)
+return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	bytesRead = read(fd, buffer, letters);
+
+	if (bytesRead == -1)
+		return (0);
+
+	bytesWrite = write(STDOUT_FILENO, buffer, bytesRead);
+
+	if (bytesWrite == -1)
+		return (0);
+
+	close(filename);
+	free(buffer);
+
+	return (bytesWrite);
+}
