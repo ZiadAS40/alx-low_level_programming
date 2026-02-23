04cc115103fc365e3ab3c375fcec5d6cf51f5ca6
ZiadAS40 <ziad403612@gmail.com>
2026-02-23 11:51:37 -0800

    submiting the file for the task 1

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -21,22 +21,23 @@
      char *key;
      char *value;
      struct hash_node_s *next;
-    } hash_node_t;
+} hash_node_t;
     
-    /**
-    * struct hash_table_s - Hash table data structure
-    *
-    * @size: The size of the array
-    * @array: An array of size @size
-    * Each cell of this array is a pointer to the first node of a linked list,
-    * because we want our HashTable to use a Chaining collision handling
-    */
-    typedef struct hash_table_s
-    {
-        unsigned long int size;
-        hash_node_t **array;
-    } hash_table_t;
+/**
+* struct hash_table_s - Hash table data structure
+*
+* @size: The size of the array
+* @array: An array of size @size
+* Each cell of this array is a pointer to the first node of a linked list,
+* because we want our HashTable to use a Chaining collision handling
+*/
+typedef struct hash_table_s
+{
+    unsigned long int size;
+    hash_node_t **array;
+} hash_table_t;
     
-    /* function prototypes */
-    hash_table_t *hash_table_create(unsigned long int size);
+/* function prototypes */
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #endif
